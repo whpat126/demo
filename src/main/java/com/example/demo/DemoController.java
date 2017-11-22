8cd644dbb83f6f5209ea4f20527b4184ea995c68
whpat126 <124653847@qq.com>
2017-11-22 11:21:02 +0800

    use one controller

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -8,17 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/")
 public class DemoController {
 
-    @RequestMapping("/hello")
+    @RequestMapping({"/hello","/"})
     public String test(Model model) {
         String welcome = "你好！";
         model.addAttribute("welcomeString", welcome);
         return "home";
     }
 
-    @RequestMapping("/")
-    public String welcom(Model model) {
-        String welcome = "你好！";
-        model.addAttribute("welcomeString", welcome);
-        return "home";
-    }
+
 }
